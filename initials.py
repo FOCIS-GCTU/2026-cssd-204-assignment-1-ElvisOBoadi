3e3aa86e1aefe90bd3e99b2c432fc8d62f97cfa4
ElvisOBoadi <2425402980@live.gctu.edu.gh>
2026-05-30 19:36:12 +0000

    Add function to display initials in large letters
    Implement main function to print initials 'EOB' in stylized format.

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials EOB in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Elvis Owusu Boadi
+# STUDENT ID: 2425402980
+# Email: 2425402980@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Elvis Owusu Boadi, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+     
+    print()
+    print("...EOB")
+    print()
+    print("...EEEEEEEEEEEE........OOOOOOOOOOOO........BBBBBBBBBBBB...")
+    print("...EEEEEEEEEEEE........OOOOOOOOOOOO........BBBBBBBBBBBB...")
+    print("...EE..................OO........OO........BB........BB...")
+    print("...EE..................OO........OO........BB........BB...")
+    print("...EEEEEEEEEEEE........OO........OO........BBBBBBBBBBBB...")
+    print("...EEEEEEEEEEEE........OO........OO........BBBBBBBBBBBB...")
+    print("...EE..................OO........OO........BB........BB...")
+    print("...EE..................OO........OO........BB........BB...")
+    print("...EEEEEEEEEEEE...**...OOOOOOOOOOOO...**...BBBBBBBBBBBB...**")
+    print("...EEEEEEEEEEEE...**...OOOOOOOOOOOO...**...BBBBBBBBBBBB...**")
+    print()
 
 
 main()
